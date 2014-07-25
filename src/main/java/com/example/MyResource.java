d9b6c36844afe4e599f91edc3c2d885766b8fa0d
Gagan Bindra <itsgaganbindra@gmail.com>
2014-07-25 14:01:05 +0530

    revert prev changes

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -1,23 +1,23 @@
 package com.example;
 
 
-import java.net.URI;
+//import java.net.URI;
 import java.util.Set;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
+
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Request;
+//import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
 
-import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpSession;
 
 
 
@@ -41,21 +41,9 @@ public class MyResource {
     }
     
     @POST
-    public String postIt(@PathParam("fileName") String fileName, @Context UriInfo ui){
-    	
-    	MultivaluedMap<String, String> queryParams = ui.getQueryParameters();
-        MultivaluedMap<String, String> pathParams = ui.getPathParameters();
+    public String postIt(@PathParam("fileName") String fileName){
         
-        Set queryKeySet = queryParams.keySet();
-        String qParams = "";
-        
-        for(int i=0; i<=queryKeySet.size();i++){
-        	
-        qParams = qParams + queryParams.get(queryKeySet).get(i);
-        
-        }
-        
-    	return "Hello, Heroku receieved file - "+fileName+" successfully witj queryParams as "+qParams;
+    	return "Hello, Heroku receieved file - "+fileName+" successfully";
     	
     	
     }
