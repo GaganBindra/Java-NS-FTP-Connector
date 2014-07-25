26f8478198a56b99692a419ead14d3029e58e373
Gagan Bindra <itsgaganbindra@gmail.com>
2014-07-25 14:59:31 +0530

    updated for upload functionality

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -2,10 +2,14 @@ package com.example;
 
 
 //import java.net.URI;
+import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
@@ -18,10 +22,12 @@ import javax.ws.rs.core.MultivaluedMap;
 //import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-//import javax.servlet.http.HttpSession;
 
-
+import javax.servlet.http.HttpSession;
 
 
 /**
@@ -43,26 +49,62 @@ public class MyResource {
     }
     
     @POST
-    public String postIt(@PathParam("fileName") String fileName, @Context UriInfo ui){
+    public String postIt(@PathParam("fileName") String fileName, HttpServletRequest request, HttpServletResponse response){
     	
-    	MultivaluedMap<String, String> queryParams = ui.getQueryParameters();
-        MultivaluedMap<String, String> pathParams = ui.getPathParameters();
+
+         
         
-       Set queryKeySet = queryParams.keySet();
-       String queryString = "";
-       Iterator i = queryKeySet.iterator();
+//       Set queryKeySet = queryParams.keySet();
+//       String queryString = "";
+//       Iterator i = queryKeySet.iterator();
+//       
+//       while(i.hasNext()){
+//    	   
+//    	   List<String> tempList = queryParams.get(i.next());
+//    	   
+//    	   for(int j=0;j<tempList.size();j++){
+//    		   queryString = queryString.concat(tempList.get(j));   
+//    	   }
+//    	   
+//       }
+    	
+    	
+    	HttpSession session = request.getSession(true);
+    	String name = "";
+    	//process only if its multipart content
+    	if(ServletFileUpload.isMultipartContent(request))
+    	{
+            try {
+                List<FileItem> multiparts = new ServletFileUpload(
+                                         new DiskFileItemFactory()).parseRequest(request);
+              
+                for(FileItem item : multiparts){
+                    if(!item.isFormField()){
+                        name = new File(item.getName()).getName();
+                        //item.write( new File(UPLOAD_DIRECTORY + File.separator + name));
+                    }
+                }
+           
+               //File uploaded successfully
+               //request.setAttribute("message", "Ahaan! File Uploaded Successfully.");
+             
+//               DataBean bean = ReadCSVFile.main(null);
+//               
+//               String rowCount = String.valueOf((bean.getEmail()).size());
+//               
+//               session.setAttribute("bean", bean);
+//               session.setAttribute("rowCount", rowCount);
+               
+            } catch (Exception ex) {
+               request.setAttribute("message", "File Upload Failed due to " + ex);
+               return "File Upload Failed due to " + ex;
+            }          
+         
+        }else{
+            return "Sorry this Servlet only handles file upload request";
+        }
        
-       while(i.hasNext()){
-    	   
-    	   List<String> tempList = queryParams.get(i.next());
-    	   
-    	   for(int j=0;j<tempList.size();j++){
-    		   queryString = queryString.concat(tempList.get(j));   
-    	   }
-    	   
-       }
-       
-    	return "Hello, Heroku receieved file - "+fileName+" successfully with queryParam = "+queryString;
+    	return "Hello, Heroku receieved file - "+name+" successfully,and fileName param is "+fileName;
     	
     	
     }
