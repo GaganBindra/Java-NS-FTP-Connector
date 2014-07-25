9abab9db8f56f4f6b5990fbb09a64bea9389d1e2
Gagan Bindra <itsgaganbindra@gmail.com>
2014-07-25 15:53:35 +0530

    updated

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -3,6 +3,7 @@ package com.example;
 
 //import java.net.URI;
 import java.io.File;
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -13,18 +14,23 @@ import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 //import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 
 import javax.servlet.http.HttpSession;
@@ -49,64 +55,50 @@ public class MyResource {
     }
     
     @POST
-    public String postIt(@PathParam("fileName") String fileName, HttpServletRequest request, HttpServletResponse response){
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String postIt(@FormDataParam("file") InputStream uploadedInputStream,
+    		@FormDataParam("file") FormDataContentDisposition fileDetail) {
     	
-
-         
-        
-//       Set queryKeySet = queryParams.keySet();
-//       String queryString = "";
-//       Iterator i = queryKeySet.iterator();
-//       
-//       while(i.hasNext()){
-//    	   
-//    	   List<String> tempList = queryParams.get(i.next());
-//    	   
-//    	   for(int j=0;j<tempList.size();j++){
-//    		   queryString = queryString.concat(tempList.get(j));   
-//    	   }
-//    	   
-//       }
-    	
-    	
-    	HttpSession session = request.getSession(true);
     	String name = "";
-    	//process only if its multipart content
-    	if(ServletFileUpload.isMultipartContent(request))
-    	{
-            try {
-                List<FileItem> multiparts = new ServletFileUpload(
-                                         new DiskFileItemFactory()).parseRequest(request);
-              
-                for(FileItem item : multiparts){
-                    if(!item.isFormField()){
-                        name = new File(item.getName()).getName();
-                        //item.write( new File(UPLOAD_DIRECTORY + File.separator + name));
-                    }
-                }
-           
-               //File uploaded successfully
-               //request.setAttribute("message", "Ahaan! File Uploaded Successfully.");
-             
-//               DataBean bean = ReadCSVFile.main(null);
-//               
-//               String rowCount = String.valueOf((bean.getEmail()).size());
-//               
-//               session.setAttribute("bean", bean);
-//               session.setAttribute("rowCount", rowCount);
-               
-            } catch (Exception ex) {
-               request.setAttribute("message", "File Upload Failed due to " + ex);
-               return "File Upload Failed due to " + ex;
-            }          
-         
-        }else{
-            return "Sorry this Servlet only handles file upload request";
-        }
-       
-    	return "Hello, Heroku receieved file - "+name+" successfully,and fileName param is "+fileName;
+  
+    	String uploadedFileLocation = "d://uploaded/" + fileDetail.getFileName();
+ 
+		
+		
+		// save it
+		//writeToFile(uploadedInputStream, uploadedFileLocation);
+ 
+		String output = "File uploaded to : " + uploadedFileLocation;
+		
+		return output;
+ 
+		//return Response.status(200).entity(output).build();
+    	
+    	//return "Hello, Heroku receieved file - "+name+" successfully,and fileName param is "+fileName;
     	
     	
     }
+    
+    @POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+		@FormDataParam("file") InputStream uploadedInputStream,
+		@FormDataParam("file") FormDataContentDisposition fileDetail) {
+ 
+		String uploadedFileLocation = "d://uploaded/" + fileDetail.getFileName();
+ 
+		
+		
+		// save it
+		//writeToFile(uploadedInputStream, uploadedFileLocation);
+ 
+		String output = "File uploaded to : " + uploadedFileLocation;
+		
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
     
 }
