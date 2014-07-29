a792b5acd80412d680df883cfa192abb5b576000
Gagan Bindra <itsgaganbindra@gmail.com>
2014-07-29 20:30:33 +0530

    updated

--- src/main/java/com/example/MyResource2.java
+++ src/main/java/com/example/MyResource2.java
@@ -27,8 +27,7 @@ public class MyResource2 {
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces("text/plain")
-	public String handleUpload(@FormDataParam("file") InputStream in,
-	                           @FormDataParam("file") FormDataContentDisposition fileDetail) throws Exception {
+	public String handleUpload(@FormDataParam("file") FormDataContentDisposition fileDetail) throws Exception {
 
 	    if(fileDetail==null || fileDetail.getFileName()==null) {
 	        return "No filename";
